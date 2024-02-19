69aef55926ebde326c2839a5845055e291e341e6
Phi <phihdn@gmail.com>
2024-02-19 16:55:26 -0400

    serve static files

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	// Serve static files
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		template.Dashboard.ExecuteWriter(pongo2.Context{}, w)
 	})
