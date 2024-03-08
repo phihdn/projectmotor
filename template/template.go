dec71475ee98489c807fb6b918e401fde11af7d1
Phi <phihdn@gmail.com>
2024-03-07 21:23:42 -0400

    Add auth templates (with guest layout)

--- template/template.go
+++ template/template.go
@@ -4,5 +4,10 @@ import "github.com/flosch/pongo2/v6"
 
 var set = pongo2.NewSet("default", pongo2.MustNewLocalFileSystemLoader("./views"))
 
-// Dashboard
-var Dashboard *pongo2.Template = pongo2.Must(set.FromFile("dashboard/index.html"))
+var (
+	// Dashboard
+	Dashboard *pongo2.Template = pongo2.Must(set.FromFile("dashboard/index.html"))
+
+	// Login
+	Login *pongo2.Template = pongo2.Must(set.FromFile("login/index.html"))
+)
